8ae2932723c248b773baf8ae87c10d60de399c76
Forgus <wenbin1876@gmail.com>
2022-09-24 07:59:05 +0100

    ..

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function install_screenfetch() {
+install_screenfetch() {
 	wget https://github.com/KittyKatt/screenFetch/archive/master.zip &&
 	unzip master.zip &&
 	sudo mv screenFetch-master/screenfetch-dev /usr/bin/screenfetch &&
@@ -8,7 +8,7 @@ function install_screenfetch() {
 }
 
 # Identify the different distributions and call the corresponding function
-function multi_distro_installation() {
+multi_distro_installation() {
     if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
         install_dependencies_with_yum 
     elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
@@ -22,38 +22,56 @@ function multi_distro_installation() {
     else
         echo "Non-supported operating system version"
     fi
-	install_screenfetch
+#	install_screenfetch
 }
-function install_dependencies_with_yum() {
+install_dependencies_with_yum() {
     sudo yum install yum-utils
 	sudo yum install -y neovim git ranger npm fish unzip
 }
-function install_dependencies_with_apt() {
-    sudo apt-get update
-    sudo apt-get install -y neovim git ranger npm fish unzip
+install_dependencies_with_apt() {
+    sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+    sudo cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
+    sudo cat >/etc/apt/sources.list<<EOF
+deb https://mirrors.ustc.edu.cn/debian/ bullseye main contrib non-free
+deb https://mirrors.ustc.edu.cn/debian/ bullseye-updates main contrib non-free
+deb https://mirrors.ustc.edu.cn/debian-security bullseye-security main contrib non-free
+EOF
+    sudo cat>/etc/apt/sources.list.d/raspi.list<<EOF
+deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ bullseye main
+EOF
+    sudo apt update
+    sudo apt full-upgrade -y
+    sudo apt install iptables fish git neovim nodejs npm python3-venv python3-pip iperf3 -y
+    sudo iptables -F
+    sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+    sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+    curl -SfL https://get.docker.com | sh -
+    sudo cat >/etc/docker/daemon.json<<EOF
+{
+  "registry-mirrors": ["https://xx0uqinw.mirror.aliyuncs.com"]
 }
-function install_dependencies_with_aur() {
-    sudo pacman -S neovim git ranger npm fish unzip --noconfirm
+EOF
+    sudo systemctl restart docker
+    
 }
-function install_dependencies_on_mac_osx() {
-    brew install neovim git ranger npm fish unzip
+
+common_config() {
+    mkdir -p ~/.config && cd ~/.config
+    echo 'starting config nvim...'
+    git clone https://gitee.com/c-w-b/nvim.git
+    cd ~
 }
 
 # Entry
-function main() {
+main() {
     OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
-    if [[ "${OS_NAME}" == "linux" ]]; then
+    if [ "${OS_NAME}" = "linux" ]
+    then
         multi_distro_installation
-    elif [[ "${OS_NAME}" == "darwin" ]]; then
-        install_dependencies_on_mac_osx
     else
         echo "Non-surported distribution"
     fi
-	npm config set registry https://registry.npm.taobao.org &&
-	mkdir -p ~/.config && cd ~/.config &&
-	git clone https://github.com/Forgus/nvim.git &&
-	cd nvim && git checkout core-qwerty &&
-	fish
+    #common_config
 }
 
 main
